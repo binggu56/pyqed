b4bd6db0105715f2eef38c67bccd57b5fdb85c96
czh <pb142060@mail.ustc.edu.cn>
2023-08-01 22:03:28 +0800

    add prony (numerical)

--- examples/prony.py
+++ examples/prony.py
@@ -2,7 +2,7 @@
 This example shows how to use the DEOM class to simulate the dynamics of a two-level system coupled to a drude bath.
 '''
 from pyqed.mol import Mol, LVC, Mode
-from pyqed.deom import Bath, decompose_specturm_prony, spectrum_exp, decompose_specturm_pade
+from pyqed.deom import Bath, decompose_spectrum_prony, spectrum_exp, decompose_spectrum_pade, prony_fitting
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,6 +30,11 @@ def pos(n_vib):
     return X
 
 
+def gen_jw(w, lams1, gams1):
+    return w * lams1 * np.exp(- gams1 * np.abs(w))
+
+
+# genarate eta and gamma from spectrum
 gams = 1 / (50 / 2.41888432651e-2)
 lamd = 10.6 / (219474.6305)
 print(gams, lamd)
@@ -39,9 +44,9 @@ sp_para_dict = {lamd_sp: lamd, gams_sp: gams}
 phixx_sp = (lamd_sp * gams_sp * gams_sp / (gams_sp *
             gams_sp + w_sp * w_sp)).subs(sp_para_dict)
 
-etal_pade, etar_pade, etaa_pade, expn_pade = decompose_specturm_prony(
+etal_pade, etar_pade, etaa_pade, expn_pade = decompose_spectrum_prony(
     phixx_sp, w_sp, 315774.6 / 3, ['a', 4], scale=25000, n=2500, npsd=100, bose_fermi=2)
-etal_pade1, etar_pade1, etaa_pade1, expn_pade1 = decompose_specturm_pade(
+etal_pade1, etar_pade1, etaa_pade1, expn_pade1 = decompose_spectrum_pade(
     phixx_sp, w_sp, 315774.6 / 3, 4, bose_fermi=2)
 len_ = 1001
 spe_wid = 0.05
@@ -55,5 +60,47 @@ spe = (phixx / (1 + np.exp(315774.6 / 3 * w)))
 plt.plot(w, spe, 'g', label='phixx')
 plt.plot(w, res_spe.real, 'b', label='prony')
 plt.plot(w, res_spe1.real, 'r--', label='pade')
+plt.legend(loc='best')
+plt.show()
+plt.clf()
+
+# genarate eta and gamma from time correlation function
+n = 1000
+scale = 200
+beta = 1
+gams1 = 1
+lams1 = 1
+n_fft = 1000000
+scale_fft = 2000
+
+n_rate = (scale_fft * scale / (4 * n))
+n_rate = int(n_rate)
+w = np.linspace(0, scale_fft * np.pi, n_fft + 1)[:-1]
+dw = w[1] - w[0]
+jw = gen_jw(w, lams1, gams1)
+cw1 = jw / (1 - np.exp(-beta * w))
+cw2 = jw / (1 - np.exp(+beta * w))
+del jw
+
+cw1[0] = cw1[1] / 2
+cw2[0] = cw2[1] / 2
+fft_ct = (np.fft.fft(cw1) * dw - np.fft.ifft(cw2) * len(cw2) * dw) / np.pi
+fft_t = 2 * np.pi * np.fft.fftfreq(len(cw1), dw)
+del cw1, cw2
+
+fft_ct = fft_ct[(scale >= fft_t) & (fft_t >= 0)][::n_rate]
+fft_t = fft_t[(scale >= fft_t) & (fft_t >= 0)][::n_rate]
+etal, _, _, expn = prony_fitting(fft_ct, fft_t, [3, 3], scale, n)
+
+len_ = 10000
+spe_wid = 20
+
+w = np.append(np.linspace(-spe_wid, 0, len_),
+              np.linspace(0, spe_wid, len_))
+phixx = w * lams1 * np.exp(- gams1 * np.abs(w)) / (1 - np.exp(-beta * w))
+res_spe = np.zeros(len(w), dtype=complex)
+spectrum_exp(w, res_spe, expn, etal)
+plt.plot(w, phixx, 'g', label='phixx')
+plt.plot(w, res_spe.real, 'b--', label='prony')
 plt.legend(loc='best')
 plt.show()
