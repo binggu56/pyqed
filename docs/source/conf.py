1daefce314129213736a3472171d40cf804f56bb
Bing Gu <binggu56@gmail.com>
2023-10-17 22:46:57 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '1.0.0'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax'
 ]
 
-.. autoclass:: lime.oqs.LindbladSolver
+#.. autoclass:: lime.oqs.LindbladSolver
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
